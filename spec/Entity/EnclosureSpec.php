716e82099909a1ad1cac34bd0f9ba03cec627903
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-05 19:03:13 +0200

    enclosure

---
+++ spec/Entity/EnclosureSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace spec\App\Entity;
+
+use App\Entity\Dinosaur;
+use App\Entity\Enclosure;
+use App\Factory\DinosaurFactory;
+use PhpSpec\ObjectBehavior;
+
+class EnclosureSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Enclosure::class);
+    }
+
+    function it_should_have_no_dinosaurs_by_default()
+    {
+        $this->getDinosaurs()->shouldHaveCount(0);
+    }
+
+    function it_should_be_able_to_add_dinosaurs()
+    {
+        $this->addDinosaur(new Dinosaur());
+        $this->addDinosaur(new Dinosaur());
+
+        $this->getDinosaurs()->shouldHaveCount(2);
+        
+
+    }
+}
