8a1166261650ce4eb1dea558247f8d157879e4b0
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-06 09:17:43 +0200

    rename with content

---
+++ spec/Service/EnclosureBuilderServiceSpec.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace spec\App\Service;
+
+use App\Entity\Dinosaur;
+use App\Entity\Enclosure;
+use App\Factory\DinosaurFactory;
+use App\Service\EnclosureBuilderService;
+use PhpSpec\ObjectBehavior;
+
+class EnclosureBuilderServiceSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(EnclosureBuilderService::class);
+    }
+
+
+
+    function it_builds_enclosure_with_dinosaurs(DinosaurFactory $dinosaurFactory)
+    {
+
+        
+        $this->beConstructedWith($dinosaurFactory);
+        $enclosure = $this->buildEnclosure(1, 2);
+
+        $enclosure->shouldBeAnInstanceOf(Enclosure::class);
+        $enclosure->isSecurityActive()->shouldReturn(true);
+    }
+}
